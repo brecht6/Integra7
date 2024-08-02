4d61e611de76a7d46eed78bde5811c56ef494d5c
Stefaan Himpe <stefaan.himpe@gmail.com>
2024-08-02 22:18:00 +0200

    extract preset selector into a user control exposing a Presets property

---
+++ Views/PresetSelector.axaml.cs
@@ -0,0 +1,21 @@
+using System.Collections.ObjectModel;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Integra7AuralAlchemist.Models.Data;
+
+namespace Integral7AuralAlchemist.Views;
+
+public partial class PresetSelector : UserControl
+{
+    public static readonly StyledProperty<ReadOnlyObservableCollection<Integra7Preset>> PresetsProperty = 
+        AvaloniaProperty.Register<PresetSelector, ReadOnlyObservableCollection<Integra7Preset>>(nameof(Presets));
+    public ReadOnlyObservableCollection<Integra7Preset> Presets {
+        get => (ReadOnlyObservableCollection<Integra7Preset>)GetValue(PresetsProperty);
+        set => SetValue(PresetsProperty, value);
+    }
+    public PresetSelector()
+    {
+        InitializeComponent();
+    }
+}

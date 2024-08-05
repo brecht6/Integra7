71f1a5846033cdf35b56d3a0e20945fbb96b4745
Stefaan Himpe <stefaan.himpe@gmail.com>
2024-08-05 23:30:20 +0200

    some initial work on sysex - will need to add some unit tests first

--- Models/Services/ByteUtils.cs
+++ Models/Services/ByteUtils.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Diagnostics;
 using System.Linq;
 
 namespace Integra7AuralAlchemist.Models.Services;
@@ -14,5 +16,67 @@ public class ByteUtils
             offset += array.Length;
         }
         return rv;
+    }
+
+    public static byte LtlEnd_FirstByte7(long value)
+    {
+        Debug.Assert(value < 0x80000000);
+        return (byte)(value & 0x7f);
+    }
+
+    public static byte LtlEnd_SecondByte7(long value)
+    {
+        Debug.Assert(value < 0x80000000);
+        return (byte)((value >> 7) & 0x7f);
+    }
+
+    public static byte LtlEnd_ThirdByte7(long value)
+    {
+        Debug.Assert(value < 0x80000000);
+        return (byte)((value >> 14) & 0x7f);
+    }
+
+    public static byte LtlEnd_FourthByte7(long value)
+    {
+        Debug.Assert(value < 0x80000000);
+        return (byte)((value >> 21) & 0x7f);
+    }
+
+    public static byte[] IntToBytes7(long value)
+    {
+        Debug.Assert(value < 0x80000000);
+        return [LtlEnd_FourthByte7(value), LtlEnd_ThirdByte7(value),
+                LtlEnd_SecondByte7(value), LtlEnd_FirstByte7(value)];
+    }
+
+    public static byte[] AddressWithOffset(byte[] StartAddress, byte[] Offset)
+    {
+        byte[] result = new byte[StartAddress.Length];
+        Array.Copy(StartAddress, result, StartAddress.Length);
+        Debug.Assert(StartAddress.Length >= Offset.Length);
+        for (int i = 0; i < Offset.Length; i++)
+        {
+            result[StartAddress.Length - 1 - i] = (byte)(result[StartAddress.Length - 1 - i] + Offset[Offset.Length - 1 - i]);
+        }
+        return result;
+    }
+
+    public static long Bytes7ToInt(byte[] data)
+    {
+        Debug.Assert(data.Length == 4);
+        return ((((((long)data[0] << 7) + (long)data[1]) << 7) + (long)data[2]) << 7) + (long)data[3];
+    }
+
+    public static byte CheckSum(byte[] data)
+    {
+        int sum = 0;
+        for (int i = 0; i < data.Length; i++)
+        {
+            sum += data[i];
+            sum &= 0xff;
+        }
+        int remainder = (sum % 128);
+        int checksum = 128 - remainder;
+        return (byte)checksum;
     }
 }
--- Models/Services/Integra7SysexHelpers.cs
+++ Models/Services/Integra7SysexHelpers.cs
@@ -1,7 +1,5 @@
-using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
-using Avalonia.Controls;
-using ReactiveUI;
 
 namespace Integra7AuralAlchemist.Models.Services;
 public class Integra7SysexHelpers
@@ -16,8 +14,11 @@ public class Integra7SysexHelpers
     private static byte[] ROLAND_DEVICE_FAMILY_CODE = [0x64, 0x02];
     private static byte[] ROLAND_DEVICE_FAMILY_NUMBER_CODE = [0x00, 0x00];
     private static byte[] ROLAND_DEVICE_FAMILY_SW_REV = [0x00, 0x00, 0x00, 0x00];
+    private static byte[] MODEL_ID = [0x00, 0x00, 0x64];
     private static byte[] END_OF_SYSEX = [0xF7];
     private static byte[] DEVICE_ID = [0x10];
+    private static byte[] COMMAND_DATAREQ = [0x11];
+    private static byte[] COMMAND_DATASET = [0x12];
 
     public static byte[] IDENTITY_REQUEST = ByteUtils.Flatten([ EXCLUSIVE_STATUS, UNIVERSAL_NON_RT, SYSEX_GLOBAL_CH,
                                        IDENTITY_GEN_INFO, IDENTITY_ID_REQ, END_OF_SYSEX ]);
@@ -25,6 +26,101 @@ public class Integra7SysexHelpers
                                     IDENTITY_GEN_INFO, IDENTITY_ID_REP, ROLAND_ID,
                                     ROLAND_DEVICE_FAMILY_CODE, ROLAND_DEVICE_FAMILY_NUMBER_CODE, ROLAND_DEVICE_FAMILY_SW_REV,
                                     END_OF_SYSEX]);
+
+    public static byte[] StartAddress_Setup = [0x01, 0x00, 0x00, 0x00];
+    public static byte[] StartAddress_System = [0x02, 0x00, 0x00, 0x00];
+    public static byte[] StartAddress_TempStudioSet = [0x18, 0x00, 0x00, 0x00];
+    public static byte[] StartAddress_TemporaryTone_Part1 = [0x19, 0x00, 0x00, 0x00];
+    public static byte[] StartAddress_TemporaryTone_Part2 = [0x19, 0x20, 0x00, 0x00];
+    public static byte[] StartAddress_TemporaryTone_Part3 = [0x19, 0x40, 0x00, 0x00];
+    public static byte[] StartAddress_TemporaryTone_Part4 = [0x19, 0x60, 0x00, 0x00];
+    public static byte[] StartAddress_TemporaryTone_Part5 = [0x1A, 0x00, 0x00, 0x00];
+    public static byte[] StartAddress_TemporaryTone_Part6 = [0x1A, 0x20, 0x00, 0x00];
+    public static byte[] StartAddress_TemporaryTone_Part7 = [0x1A, 0x40, 0x00, 0x00];
+    public static byte[] StartAddress_TemporaryTone_Part8 = [0x1A, 0x60, 0x00, 0x00];
+    public static byte[] StartAddress_TemporaryTone_Part9 = [0x1B, 0x00, 0x00, 0x00];
+    public static byte[] StartAddress_TemporaryTone_Part10 = [0x1B, 0x20, 0x00, 0x00];
+    public static byte[] StartAddress_TemporaryTone_Part11 = [0x1B, 0x40, 0x00, 0x00];
+    public static byte[] StartAddress_TemporaryTone_Part12 = [0x1B, 0x60, 0x00, 0x00];
+    public static byte[] StartAddress_TemporaryTone_Part13 = [0x1C, 0x00, 0x00, 0x00];
+    public static byte[] StartAddress_TemporaryTone_Part14 = [0x1C, 0x20, 0x00, 0x00];
+    public static byte[] StartAddress_TemporaryTone_Part15 = [0x1C, 0x40, 0x00, 0x00];
+    public static byte[] StartAddress_TemporaryTone_Part16 = [0x1C, 0x60, 0x00, 0x00];
+    public static byte[] Offset_System_Common = [0x00, 0x00, 0x00];
+    public static byte[] Offset_TemporaryTone_PCM_Synth_Tone = [0x00, 0x00, 0x00];
+    public static byte[] Offset_TemporaryTone_SN_Synth_Tone = [0x01, 0x00, 0x00];
+    public static byte[] Offset_TemporaryTone_SN_Acoustic_Tone = [0x02, 0x00, 0x00];
+    public static byte[] Offset_TemporaryTone_SN_Drum_Kit = [0x03, 0x00, 0x00];
+    public static byte[] Offset_TemporaryTone_PCM_Drum_Kit = [0x10, 0x00, 0x00];
+    public static byte[] Offset_StudioSet_Common = [0x00, 0x00, 0x00];
+    public static byte[] Offset_StudioSet_Common_Chorus = [0x00, 0x04, 0x00];
+    public static byte[] Offset_StudioSet_Common_Reverb = [0x00, 0x06, 0x00];
+    public static byte[] Offset_StudioSet_Common_Motional_Surround = [0x00, 0x08, 0x00];
+    public static byte[] Offset_StudioSet_Master_EQ = [0x00, 0x09, 0x00];
+    public static byte[] Offset_StudioSet_MIDI_Ch(byte ZeroBasedChannel)
+    {
+        byte[] final = [0x00, 0x10, 0x00];
+        for (byte b = 0; b < ZeroBasedChannel; b++)
+        {
+            final = ByteUtils.AddressWithOffset(final, [0x01, 0x00]);
+        }
+        return final;
+    }
+    public static byte[] Offset_StudioSet_Part(byte ZeroBasedPart)
+    {
+        byte[] final = [0x00, 0x20, 00];
+        for (byte b = 0; b < ZeroBasedPart; b++)
+        {
+            final = ByteUtils.AddressWithOffset(final, [0x01, 0x00]);
+        }
+        return final;
+    }
+    public static byte[] Offset_StudioSet_Part_EQ(byte ZeroBasedPart)
+    {
+        byte[] final = [0x00, 0x50, 0x00];
+        for (byte b = 0; b < ZeroBasedPart; b++)
+        {
+            final = ByteUtils.AddressWithOffset(final, [0x01, 0x00]);
+        }
+        return final;
+    }
+
+    public static byte[] Offset_PCM_SynthTone_Common = [0x00, 0x00, 0x00];
+    public static byte[] Offset_PCM_SynthTone_Common_MFX = [0x00, 0x02, 0x00];
+    public static byte[] Offset_PCM_SynthTone_PartialMixTable = [0x00, 0x10, 0x00];
+    public static byte[] Offset_PCM_SynthTone_Partial_1 = [0x00, 0x20, 0x00];
+    public static byte[] Offset_PCM_SynthTone_Partial_2 = [0x00, 0x22, 0x00];
+    public static byte[] Offset_PCM_SynthTone_Partial_3 = [0x00, 0x24, 0x00];
+    public static byte[] Offset_PCM_SynthTone_Partial_4 = [0x00, 0x26, 0x00];
+    public static byte[] Offset_PCM_SynthTone_Common_2 = [0x00, 0x30, 0x00];
+
+    public static byte[] Offset_PCM_DrumKit_Common = [0x00, 0x00, 0x00];
+    public static byte[] Offset_PCM_DrumKit_Common_MFX = [0x00, 0x02, 0x00];
+    public static byte[] Offset_PCM_DrumKit_Common_COMP_EQ = [0x00, 0x08, 0x00];
+
+    public static byte[] Offset_PCM_DrumKit_Partial_Key(int KeyNumber)
+    {
+        if (KeyNumber >= 21 && KeyNumber <= 76)
+        {
+            byte[] base_offset = [0x00, 0x10, 0x00];
+            for (int i=0; i<(KeyNumber-21); i++)
+            {
+                base_offset = ByteUtils.AddressWithOffset(base_offset, [0x02, 0x00]);
+            }
+            return base_offset;
+        }
+        else if (KeyNumber >= 77 && KeyNumber <= 108)
+        {
+            byte[] base_offset = [0x01, 0x00, 0x00];
+            for (int i=0; i<(KeyNumber-77); i++)
+            {
+                base_offset = ByteUtils.AddressWithOffset(base_offset, [0x02, 0x00]);
+            }
+            return base_offset;
+        }
+        Debug.Assert(false); // invalid key number specified...
+        return [];
+    }
 
     public static bool CheckIdentityReply(byte[] reply, out byte deviceId)
     {
@@ -36,6 +132,22 @@ public class Integra7SysexHelpers
         }
         deviceId = 0;
         return false;
+    }
+
+    public static byte[] MakeDataRequest(byte deviceId, long address, long size)
+    {
+        byte[] payload = ByteUtils.Flatten([ByteUtils.IntToBytes7(address), ByteUtils.IntToBytes7(size)]);
+        byte[] data = ByteUtils.Flatten([EXCLUSIVE_STATUS, ROLAND_ID, [deviceId], MODEL_ID, 
+                                         COMMAND_DATAREQ, payload, [ByteUtils.CheckSum(payload)], END_OF_SYSEX]);
+        return data;
+    }
+
+    public static byte[] MakeDataSet(byte deviceId, long address, byte[] data)
+    {
+        byte[] payload = ByteUtils.Flatten([ByteUtils.IntToBytes7(address), data]);
+        byte[] msg = ByteUtils.Flatten([EXCLUSIVE_STATUS, ROLAND_ID, [deviceId], MODEL_ID,
+                                        COMMAND_DATASET, payload, [ByteUtils.CheckSum(payload)], END_OF_SYSEX]);
+        return msg;
     }
 
 }

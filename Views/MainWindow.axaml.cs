0c489aa866f97660f3a98d3a2a0c460253739ce4
Stefaan Himpe <stefaan.himpe@gmail.com>
2024-08-05 14:07:29 +0200

    slight restyling of ui using community fluent theme

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,11 +1,14 @@
 using Avalonia.Controls;
+using FluentAvalonia.UI.Windowing;
 
 namespace Integra7AuralAlchemist.Views;
 
-public partial class MainWindow : Window
+public partial class MainWindow : AppWindow
 {
     public MainWindow()
     {
         InitializeComponent();
+        TitleBar.ExtendsContentIntoTitleBar = true;
+        TitleBar.TitleBarHitTestType = TitleBarHitTestType.Complex;
     }
 }

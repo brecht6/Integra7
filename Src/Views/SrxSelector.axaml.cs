c7b12380788e3add3382e0a7759e75d255e98e50
Stefaan Himpe <stefaan.himpe@gmail.com>
2024-09-10 20:30:21 +0200

    move SRX selector to a user control in preparation of something better looking :)

---
+++ Src/Views/SrxSelector.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Integra7AuralAlchemist.Views;
+
+public partial class SrxSelector : UserControl
+{
+    public SrxSelector()
+    {
+        InitializeComponent();
+    }
+}

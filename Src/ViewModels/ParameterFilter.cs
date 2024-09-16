bb2d80cf95b7eecb484f7bdfbbe0ce92b00d0ade
Stefaan Himpe <stefaan.himpe@gmail.com>
2024-09-16 07:34:15 +0200

    fix parameter filter to be as flexible as the preset filter

--- Src/ViewModels/ParameterFilter.cs
+++ Src/ViewModels/ParameterFilter.cs
@@ -10,7 +10,9 @@ public class FilterProvider
 {
     public static Func<FullyQualifiedParameter, bool> ParameterFilter(string text) => par =>
     {
-        bool ReturnValue = !par.ParSpec.Reserved && (string.IsNullOrEmpty(text) || par.ParSpec.Name.Contains(text, StringComparison.CurrentCultureIgnoreCase));
+        IEnumerable<string> textParts = Regex.Split(text, @"\s+").Where(s => s != string.Empty);
+        bool ReturnValue = !par.ParSpec.Reserved && (string.IsNullOrEmpty(text) || 
+                           textParts.All(txt => par.ParSpec.Name.Contains(txt.Trim(), StringComparison.CurrentCultureIgnoreCase)));
         return ReturnValue;
     };
 
@@ -18,9 +20,9 @@ public class FilterProvider
     {
         IEnumerable<string> textParts = Regex.Split(text, @"\s+").Where(s => s != string.Empty);
         return string.IsNullOrEmpty(text) || 
-               textParts.All(txt => preset.Name.Contains(txt, StringComparison.CurrentCultureIgnoreCase)
-                                    || preset.ToneBankStr.Contains(txt, StringComparison.CurrentCultureIgnoreCase)
-                                    || preset.ToneTypeStr.Contains(txt, StringComparison.CurrentCultureIgnoreCase)
-                                    || preset.CategoryStr.Contains(txt, StringComparison.CurrentCultureIgnoreCase));
+               textParts.All(txt => preset.Name.Contains(txt.Trim(), StringComparison.CurrentCultureIgnoreCase)
+                                    || preset.ToneBankStr.Contains(txt.Trim(), StringComparison.CurrentCultureIgnoreCase)
+                                    || preset.ToneTypeStr.Contains(txt.Trim(), StringComparison.CurrentCultureIgnoreCase)
+                                    || preset.CategoryStr.Contains(txt.Trim(), StringComparison.CurrentCultureIgnoreCase));
     };
 }
